bfe267b71a562b37464020db4213bc18ffdfc70f
praveen098765 <142774112+praveen098765@users.noreply.github.com>
2023-10-25 22:21:39 +0530

    Update settings.py

--- FoodBank/settings.py
+++ FoodBank/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p#xylf*v67dvw^1k#yv@ie!xlzdqo8$1j96$4hmk2#@q*#w6yc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
